c5485f15456c04268f3b1d70a428a2b441a07117
aaronrose05 <aaronrose05@gmail.com>
2020-05-02 04:24:27 -0400

    Added scroll to top button

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -4,19 +4,21 @@ import Intro from './intro';
 import Portfolio from './portfolio';
 import Experience from './experience';
 import About from './about';
+import ScrollToTop from './scrollbutton';
 
 class Home extends React.Component{
     render(){
         return(
             <div>
-                <div class="bg"></div>
-                <div class="bg bg2"></div>
-                <div class="bg bg3"></div>
+                <div className="bg"></div>
+                <div className="bg bg2"></div>
+                <div className="bg bg3"></div>
                 <Intro />
                 <Navbar />
                 <About />
                 <Experience />
                 <Portfolio />
+                <ScrollToTop />
             </div>
         )
     }
--- portfolio/src/components/intro.js
+++ portfolio/src/components/intro.js
@@ -4,16 +4,7 @@ import './styles.css';
 class Intro extends React.Component{
     render(){
         return(
-            /* 
-            <div id="intro" className="intro">
-                <p className="intro-hello">Hi, my name is Aaron Rose</p>
-                <div className="typewriter">
-                    <div className="typewriter-text">I'm a Software Developer and Data Scientist</div>
-                </div>
-            </div>
-            */
-
-            <div className="black">
+            <div id="top" className="black">
                 <p className="shell-top-bar">/Users/aaronrose/Documents/online-portfolio/portfolio</p>
                 <div id="intro" className="intro">
                 <p className="intro-hello">Hi, my name is Aaron Rose</p>
@@ -41,7 +32,7 @@ class Intro extends React.Component{
                         <br /> <br />
                         Note that the development build is not optimized.
                         <br />
-                        To create a production build, use npm run build.
+                        To create a production build, use <strong>npm run build</strong>.
                     </div>
                 </div>
             </div>
