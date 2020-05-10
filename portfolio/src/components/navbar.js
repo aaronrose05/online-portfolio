49a24b42e0eed4f2d1477d0a6acc2cc5058b76b4
aaronrose05 <aaronrose05@gmail.com>
2020-05-10 16:40:08 -0400

    Fixed navigation for code cleanliness, removed Portfolio from Portfolio for redundency

--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -2,40 +2,23 @@ import React from 'react';
 import './styles.css';
 
 class Navbar extends React.Component{
-
-    onClickAbout(e) {
+    constructor() {
+        super();
+        this.navigate = this.navigate.bind(this); 
+     }
+     navigate(e, id) {
         e && e.preventDefault(); // to avoid the link from redirecting
-        document.getElementById("about").scrollIntoView();
-        }
-
-    onClickExperience(e) {
-        e && e.preventDefault(); // to avoid the link from redirecting
-        document.getElementById("experience").scrollIntoView();
-        }
-
-    onClickEducation(e) {
-        e && e.preventDefault(); // to avoid the link from redirecting
-        document.getElementById("education").scrollIntoView();
-        }
-
-    onClickPortfolio(e) {
-        e && e.preventDefault(); // to avoid the link from redirecting
-        document.getElementById("portfolio").scrollIntoView();
-        }
-
-    onClickContact(e) {
-        e && e.preventDefault(); // to avoid the link from redirecting
-        document.getElementById("contact").scrollIntoView();
-        }
-
+        const elementToView = document.getElementById(id);
+        elementToView.scrollIntoView(); 
+     }
     render(){
         return(
             <div className="navbar">
-                <li> <p onClick={this.onClickAbout.bind(this)}> About </p> </li>
-                <li> <p onClick={this.onClickExperience.bind(this)}> Experience </p> </li>
-                <li> <p onClick={this.onClickEducation.bind(this)}> Education </p> </li>
-                <li> <p onClick={this.onClickPortfolio.bind(this)}> Portfolio </p> </li>
-                <li> <p onClick={this.onClickContact.bind(this)}> Contact </p> </li>
+                <li> <p onClick={(e=>this.navigate(e, 'about'))}> About </p> </li>
+                <li> <p onClick={(e=>this.navigate(e, 'experience'))}> Experience </p> </li>
+                <li> <p onClick={(e=>this.navigate(e, 'education'))}> Education </p> </li>
+                <li> <p onClick={(e=>this.navigate(e, 'portfolio'))}> Portfolio </p> </li>
+                <li> <p onClick={(e=>this.navigate(e, 'contact'))}> Contact </p> </li>
             </div>
         )
     }
